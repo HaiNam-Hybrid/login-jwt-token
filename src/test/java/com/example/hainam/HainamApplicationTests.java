cc7b479a4a26a3a9860d7b589ad95f92b5f0819e
NamChi <namchilove@gmail.com>
2022-03-09 23:10:15 +0700

    Initial commit

---
+++ src/test/java/com/example/hainam/HainamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hainam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HainamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
